0cf7fed79847e14712d336d35d8e64374c5a05d4
Colin Platt <colingplatt@gmail.com>
2022-05-04 12:18:35 +0200

    initial commit

--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -4,6 +4,16 @@ pragma solidity ^0.8.13;
 import "ds-test/test.sol";
 
 contract ContractTest is DSTest {
+    
+    struct PIXEL {
+        uint8[2] index;
+        uint8[3] RGB;
+    }
+
+    struct IMAGE {
+        PIXEL[] pixels;
+    }
+    
     function setUp() public {}
 
     function testExample() public {
